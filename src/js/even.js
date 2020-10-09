0b53da0e2cad180832e0a975868051db0b689a3b
Kieran Healy <kjhealy@gmail.com>
2020-10-09 12:03:59 -0400

    js issue

--- src/js/even.js
+++ src/js/even.js
@@ -124,20 +124,20 @@ Even._initToc = function() {
   });
 };
 
-// Even.fancybox = function() {
-//     if ($.fancybox) {
-//     $('.post-content').each(function() {
-//       $(this).find('img').each(function() {
-//          $(this).wrap(`<a class="fancybox" href="${this.src}" data-fancybox="gallery" data-caption="${this.title}"></a>`);
-//       });
-//     });
+Even.fancybox = function() {
+    if ($.fancybox) {
+    // $('.post-content').each(function() {
+    //   $(this).find('img').each(function() {
+    //      $(this).wrap(`<a class="fancybox" href="${this.src}" data-fancybox="gallery" data-caption="${this.title}"></a>`);
+    //   });
+    // });
 
-//     $('.fancybox').fancybox({
-//       selector: '.fancybox',
-//       protect: true,
-//     });
-//   }
-// };
+    $('.fancybox').fancybox({
+      selector: '.fancybox',
+      protect: true,
+    });
+  }
+};
 
 Even.highlight = function() {
   const blocks = document.querySelectorAll('pre code');
